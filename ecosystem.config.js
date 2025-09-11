0bda3a9448ef66cb3ceeb86712dddae422779333
Syed Jawad <contact.syedjawad@gmail.com>
2025-09-11 17:36:47 +0500

    ci/cd

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [{
+    name: 'law',
+    script: 'node_modules/next/dist/bin/next',
+    args: 'start',
+    cwd: '/var/www/law',
+    instances: 1, // or 'max' for cluster mode
+    exec_mode: 'fork', // or 'cluster'
+    env: {
+      NODE_ENV: 'production',
+      PORT: 80,
+      NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+      NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    },
+    error_file: '/var/www/law/logs/err.log',
+    out_file: '/var/www/law/logs/out.log',
+    log_file: '/var/www/law/logs/combined.log',
+    time: true,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G'
+  }]
+}
