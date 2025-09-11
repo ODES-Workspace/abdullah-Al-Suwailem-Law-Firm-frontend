72cdac480ad594c903aa3780ea626a29a39ce4aa
sherif <sherifdevv@gmail.com>
2025-09-11 07:22:03 +0300

    apis

--- src/lib/getMetaText.ts
+++ src/lib/getMetaText.ts
@@ -1,11 +1,11 @@
 "use client";
-import { MetaField, PostTranslation } from "./types";
 import { useLang } from "@/context/LangContext";
+import { Meta, Translation } from "./types";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function GetMetaText(key: string, data: any) {
   const { lang } = useLang();
-  const meta = data?.metas?.find((t: MetaField) => t.meta_key === key);
-  return meta?.translations.find((tr: PostTranslation) => tr.locale === lang)
+  const meta = data?.metas?.find((t: Meta) => t.meta_key === key);
+  return meta?.translations.find((tr: Translation) => tr.locale === lang)
     ?.value;
 }
