cf275132570af9ebb44110d5259b8c2720a92b4f
sherif <sherifdevv@gmail.com>
2025-09-17 16:04:10 +0300

    updates

--- src/shared/HomeHero.tsx
+++ src/shared/HomeHero.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Goals from "./Goals";
 import { Meta, Translation } from "@/lib";
 import { usePost } from "@/hooks/usePost";
+import Image from "next/image";
 
 export default function HomeHero() {
   const { data, isLoading } = usePost("hero");
@@ -24,29 +25,38 @@ export default function HomeHero() {
 
   return (
     <div className="relative">
-      <div className=" h-[100vh] lg:h-[985px]   bg-no-repeat bg-center bg-cover bg-[linear-gradient(rgba(0,0,0,0.35),rgba(0,0,0,0.35)),url('/fahad.jpg')]">
-        <div className="py-30 lg:py-50 px-5  h-full ">
-          <div className=" mx-auto  flex flex-col  h-full justify-center max-w-[1233px] w-full text-white">
-            {isLoading || !data ? (
-              <div className="flex-col justify-center items-center w-full text-center">
-                {isArabic ? "جاري التحميل..." : "Loading..."}
-              </div>
-            ) : (
-              <div className="lg:max-w-[689px] gap-6 flex flex-col items-center lg:items-start text-center lg:text-start">
-                <h1
-                  className={`text-3xl md:text-4xl leading-[50px] lg:text-6xl font-bold capitalize ${
-                    isArabic ? "lg:leading-[101px]" : "lg:leading-[70px]"
-                  }`}
-                >
-                  {translation?.title}
-                </h1>
-                <p className="text-lg">{translation?.description}</p>
-
-                <div className="bg-primary py-2 px-4 text-white rounded-4xl cursor-pointer w-fit">
-                  &quot;{buttonText}&quot;
+      <div className=" h-auto lg:h-[959px]   bg-no-repeat bg-center bg-cover bg-[linear-gradient(rgba(0,0,0,0.35),rgba(0,0,0,0.35)),url('/banner.png')]">
+        <div className=" py-30  lg:py-50 px-5  h-full ">
+          <div className="mx-auto  w-full h-full flex items-center gap-10 lg:gap-20 justify-center flex-col lg:flex-row max-w-[1233px]">
+            <div className="flex flex-col   justify-center   text-white">
+              {isLoading || !data ? (
+                <div className="flex-col justify-center items-center w-full text-center">
+                  {isArabic ? "جاري التحميل..." : "Loading..."}
                 </div>
-              </div>
-            )}
+              ) : (
+                <div className="lg:max-w-[689px] gap-6 flex flex-col items-center lg:items-start text-center lg:text-start">
+                  <h1
+                    className={`text-3xl md:text-4xl leading-[50px] lg:text-6xl font-bold capitalize ${
+                      isArabic ? "lg:leading-[101px]" : "lg:leading-[70px]"
+                    }`}
+                  >
+                    {translation?.title}
+                  </h1>
+                  <p className="text-lg">{translation?.description}</p>
+
+                  <div className="bg-primary py-2 px-4 text-white rounded-4xl cursor-pointer w-fit">
+                    &quot;{buttonText}&quot;
+                  </div>
+                </div>
+              )}
+            </div>
+            <div className="relative w-[200px] h-[200px] lg:w-[345px] lg:h-[345px] ">
+              <Image
+                src={"/logo-white.svg"}
+                alt={translation?.title || ""}
+                fill
+              />
+            </div>
           </div>
         </div>
       </div>
